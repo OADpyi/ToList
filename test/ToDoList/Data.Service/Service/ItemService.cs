de7ba2eb348c5fe0bac3ded9332e1f8939a1e80c
OADpyi <buqier754966@126.com>
2021-05-23 12:43:09 +0800

    itemService

--- test/ToDoList/Data.Service/Service/ItemService.cs
+++ test/ToDoList/Data.Service/Service/ItemService.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using ToDoList.Data;
 
 namespace Data.Service
@@ -16,8 +15,8 @@ namespace Data.Service
         /// <returns></returns>
         public List<Items> GetItemsInInventory(int inventoryId)
         {
-            List<Items> items = _ctx.Items.Where(p => p.ItemOfInventoryId == inventoryId 
-            && p.State==false && p.IsOverdue==false).ToList();
+            List<Items> items = _ctx.Items.Where(p => p.ItemOfInventoryId == inventoryId
+            && p.State == false && p.IsOverdue == false).ToList();
             return items;
         }
 
@@ -28,7 +27,7 @@ namespace Data.Service
         /// <returns></returns>
         public List<Items> GetFinishedItemsInInventory(int inventoryId)
         {
-            List<Items> items = _ctx.Items.Where(p => p.ItemOfInventoryId == inventoryId && p.State==true).ToList();
+            List<Items> items = _ctx.Items.Where(p => p.ItemOfInventoryId == inventoryId && p.State == true).ToList();
             return items;
         }
 
@@ -40,7 +39,7 @@ namespace Data.Service
         public List<Items> GetOverdueItemsInInventory(int inventoryId)
         {
             List<Items> items = _ctx.Items.Where(p => p.ItemOfInventoryId == inventoryId
-            && p.State==false && p.IsOverdue==true).ToList();
+            && p.State == false && p.IsOverdue == true).ToList();
             return items;
         }
 
@@ -51,8 +50,8 @@ namespace Data.Service
         /// <returns></returns>
         public List<Items> GetAllNotFinishIteams(int userId)
         {
-            List<Items> items = _ctx.Items.Where(p => p.UserId == userId 
-            && p.State==false && p.IsOverdue==false).ToList();
+            List<Items> items = _ctx.Items.Where(p => p.UserId == userId
+            && p.State == false && p.IsOverdue == false).ToList();
             return items;
         }
 
@@ -65,7 +64,7 @@ namespace Data.Service
         public List<Items> GetAllFinishIteams(int userId)
         {
             List<Items> items = _ctx.Items.Where(p => p.UserId == userId
-            && p.State==true).ToList();
+            && p.State == true).ToList();
             return items;
         }
 
@@ -119,122 +118,6 @@ namespace Data.Service
         }
 
         /// <summary>
-        /// 添加事项
-        /// </summary>
-        /// <param name="userName"></param>
-        /// <param name="inventoryName"></param>
-        /// <param name="content"></param>
-        /// <returns></returns>
-        public bool setItem(string userName, string inventoryName, string content)
-        {
-            var user = _ctx.Users.ToList().SingleOrDefault(u => u.UserName == userName);
-            var inventory = user.Inventorys.ToList().SingleOrDefault(invent => invent.InventoryName == inventoryName);
-            if (user != null && inventory != null)
-            {
-                Items item = new Items();
-                item.ItemOfInventoryId = inventory.InventoryId;
-                item.UserId = user.Id;
-                item.CreateTime = DateTime.Now;
-                item.IsOverdue = true;
-                item.State = true;
-                item.Content = content;
-                _ctx.Items.Add(item);
-                user.Items.Add(item);
-                inventory.Items.Add(item);
-                _ctx.SaveChanges();
-                return true;
-            }
-            else
-                return false;
-        }
-        /// <summary>
-        /// 添加事项的备注
-        /// </summary>
-        /// <param name="itemId"></param>
-        /// <param name="mark"></param>
-        /// <param name="userName"></param>
-        /// <returns></returns>
-        public bool setItemMark(int itemId, string mark, string userName)
-        {
-            var user = _ctx.Users.ToList().SingleOrDefault(u => u.UserName == userName);
-            if (user != null)
-            {
-                var item = user.Items.SingleOrDefault(i => i.ItemId == itemId);
-                if (item != null)
-                {
-                    item.Mark = mark;
-                    _ctx.SaveChanges();
-                    return true;
-                }
-
-            }
-            return false;
-        }
-        /// <summary>
-        /// 添加事项的过期时间
-        /// </summary>
-        /// <param name="itemId"></param>
-        /// <param name="timeOfExpiration"></param>
-        /// <param name="userName"></param>
-        /// <returns></returns>
-        public bool setItemTimeOfExpiration(int itemId, DateTime timeOfExpiration, string userName)
-        {
-            var user = _ctx.Users.ToList().SingleOrDefault(u => u.UserName == userName);
-            if (user != null)
-            {
-                var item = user.Items.SingleOrDefault(i => i.ItemId == itemId);
-                if (item != null)
-                {
-                    item.TimeOfExpiration = timeOfExpiration;
-                    _ctx.SaveChanges();
-                    return true;
-                }
-            }
-            return false;
-        }
-        /// <summary>
-        /// 修改事项的内容
-        /// </summary>
-        /// <param name="itemId"></param>
-        /// <param name="content"></param>
-        /// <param name="userName"></param>
-        /// <returns></returns>
-        public bool setItemContent(int itemId, string content, string userName)
-        {
-            var user = _ctx.Users.ToList().SingleOrDefault(u => u.UserName == userName);
-            if (user != null)
-            {
-                var item = user.Items.SingleOrDefault(i => i.ItemId == itemId);
-                if (item != null)
-                {
-                    item.Content = content;
-                    _ctx.SaveChanges();
-                    return true;
-                }
-            }
-            return false;
-        }
-        /// <summary>
-        /// 通过id查找，修改事项内容
-        /// </summary>
-        /// <param name="itemId"></param>
-        /// <param name="content"></param>
-        /// <returns></returns>
-        public bool setItemContent(int itemId, string content)
-        {
-
-            var item = _ctx.Items.SingleOrDefault(i => i.ItemId == itemId);
-            if (item != null)
-            {
-                item.Content = content;
-                _ctx.SaveChanges();
-                return true;
-            }
-            return false;
-
-        }
-
-        /// <summary>
         /// 查找事项通过ID
         /// </summary>
         /// <param name="itemId"></param>
@@ -276,29 +159,30 @@ namespace Data.Service
             var items = _ctx.Items.Where(p => p.Content.Contains(keyword) && p.UserId == userId).ToList();
             return items;
         }
+
         /// <summary>
         /// 添加事项
         /// </summary>
         /// <returns></returns>
-        public bool AddItem(string content,DateTime timeOfExpiration, string mark,int userId,int inventoryId)
+        public bool AddItem(string content, DateTime timeOfExpiration, string mark, int userId, int inventoryId)
         {
             if (content != null)
             {
                 DateTime createTime = DateTime.Now;
-                bool isOverdue=true;
-                if (createTime<= timeOfExpiration)
+                bool isOverdue = true;
+                if (createTime <= timeOfExpiration)
                     isOverdue = false;
                 Items item = new Items()
                 {
-                    IsOverdue=isOverdue,
-                    Mark=mark,
-                    UserId=userId,
-                    ItemOfInventoryId=inventoryId,
-                    TimeOfExpiration=timeOfExpiration,
-                    CreateTime=createTime,
-                    State=false,
-                    Content=content,
-                    
+                    IsOverdue = isOverdue,
+                    Mark = mark,
+                    UserId = userId,
+                    ItemOfInventoryId = inventoryId,
+                    TimeOfExpiration = timeOfExpiration,
+                    CreateTime = createTime,
+                    State = false,
+                    Content = content,
+
                 };
                 _ctx.Items.Add(item);
                 _ctx.SaveChanges();
@@ -314,14 +198,16 @@ namespace Data.Service
             List<Items> willOverdueItems = new List<Items>();
             foreach (var item in items)
             {
-                DateTime t1 = item.CreateTime;
-                DateTime t2 = item.TimeOfExpiration;
+                DateTime nowTime = DateTime.Now;
+                DateTime expirationTime = item.TimeOfExpiration;
 
-                DateTime Convert_dt1 = Convert.ToDateTime(string.Format("{0}-{1}-{2}", t1.Year, t1.Month, t1.Day));
-                DateTime Convert_dt2 = Convert.ToDateTime(string.Format("{0}-{1}-{2}", t2.Year, t2.Month, t2.Day));
+                DateTime nowTimeConvert = Convert.ToDateTime(string.Format("{0}-{1}-{2}", 
+                    nowTime.Year, nowTime.Month, nowTime.Day));
+                DateTime expirationTimeConvert = Convert.ToDateTime(string.Format("{0}-{1}-{2}",
+                    expirationTime.Year, expirationTime.Month, expirationTime.Day));
 
-                string Days = (Convert_dt2 - Convert_dt1).Days.ToString();
-                if (Days.Equals(a))
+                string days = (expirationTimeConvert - nowTimeConvert).Days.ToString();
+                if (days.Equals(a))
                 {
                     willOverdueItems.Add(item);
                 }
